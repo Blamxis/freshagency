f629b7e0df9d988bb70890f430c3b427dcba943a
Maxime Gavinet <gavinetm26@gmail.com>
2025-01-29 09:51:01 +0100

    add es & it language

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,6 +3,8 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import fr from './locales/fr.json';
 import en from './locales/en.json';
+import es from './locales/es.json';
+import it from './locales/it.json';
 
 i18n
   .use(LanguageDetector)
@@ -14,6 +16,12 @@ i18n
       },
       en: {
         translation: en
+      },
+      es: {
+        translation: es
+      },
+      it: {
+        translation: it
       }
     },
     fallbackLng: 'fr',
